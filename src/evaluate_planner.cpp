3990ff2418f5ec7f5d901fda77c0fb533b21b475
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2020-06-09 17:19:24 +0200

    Added dynamic configuration to evaluator to specify clustering parameters, visualize both groundtruth and detection clustering

--- src/evaluate_planner.cpp
+++ src/evaluate_planner.cpp
@@ -19,16 +19,21 @@
 #include <pcl/filters/extract_indices.h>
 #include <pcl/surface/convex_hull.h>
 #include <pcl/visualization/pcl_visualizer.h>
+#include <dynamic_reconfigure/server.h>
+#include "roi_viewpoint_planner/EvaluatorConfig.h"
 #include "compute_cubes.h"
 #include "point_cloud_color_handler_clusters.h"
 
 namespace ublas = boost::numeric::ublas;
 
 pcl::visualization::PCLVisualizer *viewer = nullptr;
+int vp1 = 0, vp2 = 1;
 boost::mutex viewer_mtx;
+std::atomic_bool viewer_initialized(false);
 
 octomap_vpp::RoiOcTree* roiTree;
 boost::mutex tree_mtx;
+roi_viewpoint_planner::EvaluatorConfig config;
 
 void octomapCallback(const octomap_msgs::OctomapConstPtr& msg)
 {
@@ -86,13 +91,144 @@ struct IndexPair
 
 void visualizeLoop()
 {
+  viewer_mtx.lock();
   viewer = new pcl::visualization::PCLVisualizer("ROI viewer");
+  viewer->createViewPort(0.0, 0.0, 0.5, 1.0, vp1);
+  viewer->setBackgroundColor (0, 0, 0, vp1);
+  viewer->addText ("Groundtruth", 10, 10, "vp1cap", vp1);
+  //viewer->createViewPortCamera(vp1);
+  viewer->createViewPort(0.5, 0.0, 1.0, 1.0, vp2);
+  viewer->setBackgroundColor (0.1, 0.1, 0.1, vp2);
+  viewer->addText ("Detection", 10, 10, "vp2cap", vp2);
+  //viewer->createViewPortCamera(vp2);
+  viewer_initialized.store(true);
+  viewer_mtx.unlock();
   for (ros::Rate rate(30); ros::ok(); rate.sleep())
   {
     viewer_mtx.lock();
     viewer->spinOnce();
     viewer_mtx.unlock();
   }
+}
+
+bool clusterWithPCL(pcl::PointCloud<pcl::PointXYZ>::ConstPtr input_cloud, std::vector<pcl::PointIndices> &clusters, pcl::PointCloud<pcl::Normal>::Ptr normal_cloud = nullptr)
+{
+  if (input_cloud->empty())
+  {
+    ROS_WARN("Input cloud was emtpy; no clustering performed");
+    return false;
+  }
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  tree->setInputCloud(input_cloud);
+
+  if (config.clustering == roi_viewpoint_planner::Evaluator_EUCLIDEAN_CLUSTERING)
+  {
+    pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+    ec.setClusterTolerance (config.cluster_tolerance);
+    ec.setMinClusterSize (config.min_cluster_size);
+    ec.setMaxClusterSize (config.max_cluster_size);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (input_cloud);
+    ec.extract (clusters);
+  }
+  else if (config.clustering == roi_viewpoint_planner::Evaluator_REGION_GROWING)
+  {
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+    ne.setInputCloud(input_cloud);
+    ne.setSearchMethod(tree);
+    ne.setRadiusSearch(config.normal_search_radius);
+    normal_cloud.reset(new pcl::PointCloud<pcl::Normal>);
+    ne.compute(*normal_cloud);
+
+    pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> rg;
+    rg.setInputCloud(input_cloud);
+    rg.setSearchMethod(tree);
+    rg.setInputNormals(normal_cloud);
+    rg.setMinClusterSize(config.min_cluster_size);
+    rg.setMaxClusterSize(config.max_cluster_size);
+    rg.setSmoothnessThreshold(config.smoothness_threshold / 180.0f * M_PI);
+    rg.setCurvatureThreshold(config.curvature_threshold);
+    rg.setResidualThreshold(config.residual_threshold);
+    rg.setSmoothModeFlag(config.smooth_mode_flag);
+    rg.setCurvatureTestFlag(config.curvature_test_flag);
+    rg.setResidualTestFlag(config.residual_test_flag);
+    rg.extract(clusters);
+  }
+  else
+  {
+    ROS_ERROR("No valid clustering method was selected");
+    return false;
+  }
+  return true;
+}
+
+bool computeHullsAndVolumes(pcl::PointCloud<pcl::PointXYZ>::ConstPtr input_cloud, const std::vector<pcl::PointIndices> &clusters,
+                           std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> &hulls_clouds,
+                           std::vector<std::vector<pcl::Vertices>> &hulls_polygons,
+                           std::vector<double> &cluster_volumes
+                           )
+{
+  if (input_cloud->empty() || clusters.empty())
+    return false;
+
+  for (const pcl::PointIndices &cluster : clusters)
+  {
+    pcl::PointCloud<pcl::PointXYZ>::Ptr hull_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    std::vector<pcl::Vertices> hull_polygons;
+
+    pcl::PointIndicesPtr cluster_ptr(new pcl::PointIndices(cluster));
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::ExtractIndices<pcl::PointXYZ> extr_inds;
+    extr_inds.setInputCloud(input_cloud);
+    extr_inds.setIndices(cluster_ptr);
+    extr_inds.filter(*cluster_cloud);
+
+    pcl::ConvexHull<pcl::PointXYZ> hull;
+    hull.setInputCloud(cluster_cloud);
+    //hull.setIndices(cluster_ptr); // doesn't work -> use extract indices
+    hull.setComputeAreaVolume(true);
+
+    hull.reconstruct(*hull_cloud, hull_polygons);
+    int dim = hull.getDimension();
+    double vol;
+    if (dim == 2)
+    {
+      const double thickness = 0.01; // Assume 1 cm thickness (might be reasonable to set to octomap resolution)
+      vol = hull.getTotalArea() * thickness * 1000000; // to cm3
+    }
+    else // if dim == 3
+    {
+      vol = hull.getTotalVolume() * 1000000; // to cm3
+    }
+
+    ROS_INFO_STREAM("Hull dimension: " << dim << "Cluster volume: " << vol);
+
+    hulls_clouds.push_back(hull_cloud);
+    hulls_polygons.push_back(hull_polygons);
+    cluster_volumes.push_back(vol);
+  }
+  return true;
+}
+
+void reconfigureCallback(roi_viewpoint_planner::EvaluatorConfig &new_config, uint32_t level)
+{
+  if (new_config.min_cluster_size > new_config.max_cluster_size) // min cluster size must be smaller than max cluster size
+  {
+    if (level & (1 << 1)) // min_cluster_size was changed
+      new_config.max_cluster_size = new_config.min_cluster_size;
+    else //if (level & (1 << 2)) / max_cluster_size was changed
+      new_config.min_cluster_size = new_config.max_cluster_size;
+  }
+
+  if (!new_config.curvature_test_flag && !new_config.residual_test_flag) // either curvature or residual test must be activated
+  {
+    if (level & (1 << 9)) // residual_test_flag was changed
+      new_config.curvature_test_flag = true;
+    else if (level & (1 << 8)) // curvature_test_flag was changed
+      new_config.residual_test_flag = true;
+  }
+
+  config = new_config;
 }
 
 int main(int argc, char **argv)
@@ -102,6 +238,12 @@ int main(int argc, char **argv)
   ros::NodeHandle nhp("~");
   ros::AsyncSpinner spinner(4);
   spinner.start();
+
+  // PCL visualization init
+  boost::thread visualizeThread(visualizeLoop);
+
+  dynamic_reconfigure::Server<roi_viewpoint_planner::EvaluatorConfig> server(nhp);
+  server.setCallback(reconfigureCallback);
 
   std::string package_path = ros::package::getPath("roi_viewpoint_planner");
 
@@ -195,16 +337,39 @@ int main(int argc, char **argv)
 
   ROS_INFO_STREAM("GT tree ROI key count: " << gt_tree_keys.size());
 
+  auto isGtOcc = [](const octomap::OcTree &tree, const octomap::OcTreeNode *node) { return node->getLogOdds() > 0; };
+  pcl::PointCloud<pcl::PointXYZ>::Ptr gt_pcl = octomap_vpp::octomapToPcl<octomap::OcTree, pcl::PointXYZ>(gt_tree, isGtOcc);
+
+  std::vector<pcl::PointIndices> gt_clusters;
+  std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> gt_hulls_clouds;
+  std::vector<std::vector<pcl::Vertices>> gt_hulls_polygons;
+  std::vector<double> gt_cluster_volumes;
+
+  clusterWithPCL(gt_pcl, gt_clusters);
+  computeHullsAndVolumes(gt_pcl, gt_clusters, gt_hulls_clouds, gt_hulls_polygons, gt_cluster_volumes);
+
+  while(!viewer_initialized.load())
+  {
+    ROS_INFO("Waiting for viewer initialization...");
+    ros::Duration(0.1).sleep();
+  }
+
+  pcl::visualization::PointCloudColorHandlerClusters<pcl::PointXYZ> gt_color_handler(gt_pcl, gt_clusters);
+  viewer_mtx.lock();
+  viewer->addPointCloud<pcl::PointXYZ> (gt_pcl, gt_color_handler, "gt_cloud", vp1);
+  for (size_t i = 0; i < gt_clusters.size(); i++)
+  {
+    std::string name = "gt_cluster_" + i;
+    viewer->addPolygonMesh<pcl::PointXYZ>(gt_hulls_clouds[i], gt_hulls_polygons[i], name, vp1);
+  }
+  viewer_mtx.unlock();
+
   // Register publishers and subscribers
 
   ros::Publisher gt_pub = nhp.advertise<visualization_msgs::Marker>("roi_gt", 10, true);
   publishCubeVisualization(gt_pub, roi_locations, roi_sizes, COLOR_GREEN, "gt_rois");
 
   ros::Subscriber octomap_sub = nh.subscribe("/octomap", 10, octomapCallback);
-
-  // PCL visualization init
-
-  boost::thread visualizeThread(visualizeLoop);
 
   // Activate planner
 
@@ -213,16 +378,16 @@ int main(int argc, char **argv)
   std::ofstream resultsFile("planner_results.csv");
   resultsFile << "Time (s), Detected ROIs, ROI percentage, Average distance, Average volume accuracy, Covered ROI volume, False ROI volume, ROI key count, True ROI keys, False ROI keys" << std::endl;
 
-  roi_viewpoint_planner::PlannerConfig config;
-  if (!configClient.getCurrentConfiguration(config, ros::Duration(1)))
+  roi_viewpoint_planner::PlannerConfig planner_config;
+  if (!configClient.getCurrentConfiguration(planner_config, ros::Duration(1)))
   {
     ROS_ERROR("Could not contact configuration server");
     return -1;
   }
-  config.activate_execution = true;
-  config.require_execution_confirmation = false;
-  config.mode = planning_mode;
-  if (!configClient.setConfiguration(config))
+  planner_config.activate_execution = true;
+  planner_config.require_execution_confirmation = false;
+  planner_config.mode = planning_mode;
+  if (!configClient.setConfiguration(planner_config))
   {
     ROS_ERROR("Applying configuration not successful");
     return -1;
@@ -262,97 +427,34 @@ int main(int argc, char **argv)
 
     auto isRoi = [](const octomap_vpp::RoiOcTree &tree, const octomap_vpp::RoiOcTreeNode *node) { return tree.isNodeROI(node); };
     pcl::PointCloud<pcl::PointXYZ>::Ptr roi_pcl = octomap_vpp::octomapToPcl<octomap_vpp::RoiOcTree, pcl::PointXYZ>(*roiTree, isRoi);
-    pcl::PointCloud<pcl::Normal>::Ptr normal_cloud(new pcl::PointCloud<pcl::Normal>);
 
     tree_mtx.unlock();
 
-
     // PCL computations
     std::vector<pcl::PointIndices> clusters;
-    if (!roi_pcl->empty())
-    {
-      pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
-      tree->setInputCloud(roi_pcl);
+    std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> hulls_clouds;
+    std::vector<std::vector<pcl::Vertices>> hulls_polygons;
+    std::vector<double> cluster_volumes;
 
-      pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-      ne.setInputCloud(roi_pcl);
-      ne.setSearchMethod(tree);
-      ne.setRadiusSearch(0.03);
-      ne.compute(*normal_cloud);
-
-      /*pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> rg;
-      rg.setInputCloud(roi_pcl);
-      rg.setSearchMethod(tree);
-      rg.setInputNormals(normal_cloud);
-      rg.setMinClusterSize(5);
-      rg.setMaxClusterSize(400);
-      rg.setSmoothnessThreshold(30.0f / 180.0f * M_PI);
-      rg.setCurvatureThreshold(0.05f);
-      rg.extract(clusters);*/
-
-      pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-      ec.setClusterTolerance (0.03);
-      ec.setMinClusterSize (5);
-      ec.setMaxClusterSize (400);
-      ec.setSearchMethod (tree);
-      ec.setInputCloud (roi_pcl);
-      ec.extract (clusters);
-
-      if (viewer && !clusters.empty())
-      {
-        viewer_mtx.lock();
-        viewer->removeAllPointClouds();
-        viewer->removeAllShapes();
-        viewer_mtx.unlock();
-      }
-
-      size_t i = 0;
-      for (const pcl::PointIndices &cluster : clusters)
-      {
-        pcl::PointIndicesPtr cluster_ptr(new pcl::PointIndices(cluster));
-        pcl::PointCloud<pcl::PointXYZ>::Ptr cluster_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-        pcl::PointCloud<pcl::PointXYZ>::Ptr hull_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-        std::vector<pcl::Vertices> hull_polygons;
-
-        pcl::ExtractIndices<pcl::PointXYZ> extr_inds;
-        extr_inds.setInputCloud(roi_pcl);
-        extr_inds.setIndices(cluster_ptr);
-        extr_inds.filter(*cluster_cloud);
-
-        pcl::ConvexHull<pcl::PointXYZ> hull;
-        hull.setInputCloud(cluster_cloud);
-        //hull.setIndices(cluster_ptr);
-        hull.setComputeAreaVolume(true);
-
-        hull.reconstruct(*hull_cloud, hull_polygons);
-        int dim = hull.getDimension();
-        double vol = hull.getTotalVolume() * 1000000; // to cm3
-        ROS_INFO_STREAM("Hull dimension: " << dim << "Cluster volume: " << vol);
-
-        if (viewer && !roi_pcl->empty())
-        {
-          viewer_mtx.lock();
-          std::string name = "Cluster " + i;
-          viewer->addPolygonMesh<pcl::PointXYZ>(hull_cloud, hull_polygons, name);
-          viewer_mtx.unlock();
-        }
-        i++;
-      }
-    }
-    else
-    {
-      ROS_WARN("PCL cloud was emtpy; no clustering performed");
-    }
+    clusterWithPCL(roi_pcl, clusters);
+    computeHullsAndVolumes(roi_pcl, clusters, hulls_clouds, hulls_polygons, cluster_volumes);
 
     ROS_INFO_STREAM("Number of clusters: " << clusters.size());
 
     // Debug view pointcloud
-    if (viewer && !roi_pcl->empty())
+    if (viewer_initialized.load())
     {
       pcl::visualization::PointCloudColorHandlerClusters<pcl::PointXYZ> roi_color_handler(roi_pcl, clusters);
       viewer_mtx.lock();
-      viewer->addPointCloud<pcl::PointXYZ> (roi_pcl, roi_color_handler, "roi_cloud");
+      viewer->removeAllPointClouds(vp2);
+      viewer->removeAllShapes(vp2);
+      viewer->addPointCloud<pcl::PointXYZ> (roi_pcl, roi_color_handler, "roi_cloud", vp2);
       //viewer->addPointCloudNormals<pcl::PointXYZ, pcl::Normal> (roi_pcl, normal_cloud, 1, 0.02f, "roi_normals");
+      for (size_t i = 0; i < clusters.size(); i++)
+      {
+        std::string name = "cluster_" + i;
+        viewer->addPolygonMesh<pcl::PointXYZ>(hulls_clouds[i], hulls_polygons[i], name, vp2);
+      }
       viewer_mtx.unlock();
     }
 
@@ -502,14 +604,14 @@ int main(int argc, char **argv)
 
     if (passed_time > PLANNING_TIME) // PLANNING_TIME s timeout
     {
-      if (!configClient.getCurrentConfiguration(config, ros::Duration(1)))
+      if (!configClient.getCurrentConfiguration(planner_config, ros::Duration(1)))
       {
         ROS_ERROR("Could not contact configuration server");
         break;
       }
-      config.activate_execution = false;
-      config.mode = roi_viewpoint_planner::Planner_IDLE;
-      if (!configClient.setConfiguration(config))
+      planner_config.activate_execution = false;
+      planner_config.mode = roi_viewpoint_planner::Planner_IDLE;
+      if (!configClient.setConfiguration(planner_config))
       {
         ROS_ERROR("Applying configuration not successful");
         break;
